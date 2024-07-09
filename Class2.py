b4021e00d03aae568825b5012da0133aaf5d11b9
Marina Cantarelli <macbarroca@gmail.com>
2024-07-09 14:57:39 -0300

    If, else

---
+++ Class2.py
@@ -0,0 +1,11 @@
+#Logical operators
+
+#Boolean
+renda_acima_5mil = False
+nome_limpo = False
+
+#and / or
+if renda_acima_5mil or nome_limpo:
+    print('Financiamento aprovado')
+else: 
+    print('Financiamento negado')
