9691ae3ff4922bbf2fd172e89d8aeeef40145277
Marina Cantarelli <macbarroca@gmail.com>
2024-07-09 14:44:13 -0300

    If, else

---
+++ Class1.py
@@ -0,0 +1,12 @@
+#If, else 
+
+velocidade = 50
+if velocidade > 110: 
+    print('Acima da velocidade permitida')
+    print('Favor reduzir a sua velocidade') 
+elif velocidade  < 60:
+    print('Favor dirigir acima de 80Km/h')
+else:
+    print('velocidade OK')
+
+#print('Fim')
