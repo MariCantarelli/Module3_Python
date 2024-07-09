d20136b171775bd2442f1a738e38743207aaecae
Marina Cantarelli <macbarroca@gmail.com>
2024-07-09 19:23:41 -0300

    For loop - nested loops

---
+++ Class7.py
@@ -0,0 +1,9 @@
+#For loop - nested loops 
+
+#Outer loop
+    #Inner loop
+
+for number1 in range(1,6):
+    print("Product " + str(number1))
+    for number2 in range(11):
+        print(number1, number2)
