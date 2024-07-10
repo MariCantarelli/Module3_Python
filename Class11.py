74b42ab0dd4e876e19e71cd3eade0ac2eee7016d
Marina Cantarelli <macbarroca@gmail.com>
2024-07-10 20:47:42 -0300

    Ternary operator

---
+++ Class11.py
@@ -0,0 +1,15 @@
+#Ternary operator
+
+age = 16
+
+'''if age >= 16:
+    result = print('Voto permitido')
+else:
+   result = print('Voto não permitido')
+print(resultado)
+
+OR
+'''
+
+result = 'Voto permitido' if age >= 16 else 'Voto não permmitido'
+print(result)
